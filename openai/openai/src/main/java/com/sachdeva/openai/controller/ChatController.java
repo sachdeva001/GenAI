90821a51828fc048c6dd7267f7a7e41693f58796
Vaibhav Sachdeva <sachdeva.vaibhav.0001@gmail.com>
2026-03-22 20:44:32 +0530

    Controller added

---
+++ openai/openai/src/main/java/com/sachdeva/openai/controller/ChatController.java
@@ -0,0 +1,19 @@
+package com.sachdeva.openai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ChatController {
+	
+	
+	@GetMapping("/chat")
+	public String chat(@RequestParam("message") String message) {
+		
+		return "Success";
+	}
+
+}
