d632dd40c9780e46c7e80c6ea1573dbad5fab74a
Vaibhav Sachdeva <sachdeva.vaibhav.0001@gmail.com>
2026-03-23 20:43:39 +0530

    Controller updated with basic chatClient flow

--- openai/openai/src/main/java/com/sachdeva/openai/controller/ChatController.java
+++ openai/openai/src/main/java/com/sachdeva/openai/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.sachdeva.openai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ChatController {
 	
+	//@Autowired
+	private final ChatClient chatClient;
 	
+	public ChatController(ChatClient.Builder chatClientBuilder) {
+		this.chatClient = chatClientBuilder.build();
+	}
 	@GetMapping("/chat")
 	public String chat(@RequestParam("message") String message) {
-		
-		return "Success";
+		return chatClient.prompt(message).call().content();
 	}
 
 }
