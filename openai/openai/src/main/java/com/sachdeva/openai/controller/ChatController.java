dc9289c7220111d79de354b39d41815c7193046f
Vaibhav Sachdeva <sachdeva.vaibhav.0001@gmail.com>
2026-04-02 19:33:25 +0530

    added a new post method

--- openai/openai/src/main/java/com/sachdeva/openai/controller/ChatController.java
+++ openai/openai/src/main/java/com/sachdeva/openai/controller/ChatController.java
@@ -17,5 +17,10 @@ public class ChatController {
 		return "Success";
 	}
 	
+	@PostMapping("/chat")
+	public String chat1(@RequestParam("message") String message) {
+		
+		return "Success";
+	}
 
 }
