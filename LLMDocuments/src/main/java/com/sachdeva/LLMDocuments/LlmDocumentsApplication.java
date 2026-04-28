1dfa958eea266ab8c88f6b4aa0ec4fc96d3bbec4
Vaibhav Sachdeva <sachdeva.vaibhav.0001@gmail.com>
2026-04-28 15:27:58 +0530

    Added LLM Documents

---
+++ LLMDocuments/src/main/java/com/sachdeva/LLMDocuments/LlmDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.sachdeva.LLMDocuments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmDocumentsApplication.class, args);
+	}
+
+}
---
+++ LLMDocuments/src/test/java/com/sachdeva/LLMDocuments/LlmDocumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachdeva.LLMDocuments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmDocumentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
